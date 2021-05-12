fd2983b5ffe39cfa776d798d552ef6d8d43b4539
niko-achilles <niko.achilles.kokkinos@gmail.com>
2021-05-12 14:43:06 +0300

    search color

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ export default function Home({ stories }) {
             type="text"
             onChange={(e) => setSearchValue(e.target.value)}
             placeholder="Search stories"
-            className="px-4 py-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-white"
+            className="px-4 py-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-white text-gray-900"
           />
           <svg
             className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
