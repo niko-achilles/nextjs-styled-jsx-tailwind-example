ee3e5f7b00099a320e852c3d425ac4d70506fcbe
niko-achilles <niko.achilles.kokkinos@gmail.com>
2021-05-12 14:36:55 +0300

    corrected tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: ["./pages/**/*.js", "./components/**/*.js", "./layouts/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
