1b1df6d7bc2c412b647d621f6e7000c080477322
niko-achilles <niko.achilles.kokkinos@gmail.com>
2021-05-13 21:29:40 +0300

    lumigo settings

--- pages/_document.js
+++ pages/_document.js
@@ -34,7 +34,6 @@ class MyDocument extends Document {
             content="/favicons/browserconfig.xml"
             name="msapplication-config"
           />
-          {/* <script src="/dt.js"></script> */}
         </Head>
         <body className="bg-white text-black">
           <Main />
